ca1dd76de5425f54f1dabf7389716bcdea83a4e3
heycn <x1chennan@qq.com>
2022-08-21 18:08:03 +0800

    完成静态

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,84 @@
+import { useEffect, useRef } from 'react'
+
 export default function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const WIDTH = 600
+  const HEIGHT = 600
+
+  interface Point {
+    x: number
+    y: number
+  }
+
+  interface Branch {
+    start: Point
+    length: number
+    theta: number // 夹角角度
+  }
+
+  const init = () => {
+    const canvas = canvasRef.current
+    const ctx = canvas!.getContext('2d')
+    ctx!.strokeStyle = 'rgba(55, 65, 81, .25)'
+
+    const lineTo = (p1: Point, p2: Point) => {
+      ctx!.beginPath()
+      ctx!.moveTo(p1.x, p1.y)
+      ctx!.lineTo(p2.x, p2.y)
+      ctx!.stroke()
+    }
+
+    const getEndPoint = (b: Branch): Point => {
+      return {
+        x: b.start.x + b.length * Math.cos(b.theta),
+        y: b.start.y + b.length * Math.sin(b.theta)
+      }
+    }
+
+    const drawBranch = (b: Branch) => {
+      lineTo(b.start, getEndPoint(b))
+    }
+
+    const step = (b: Branch) => {
+      const end = getEndPoint(b)
+      drawBranch(b)
+
+      if (Math.random() < 0.5) {
+        step({
+          start: end,
+          length: b.length,
+          theta: b.theta - 0.2
+        })
+      }
+
+      if (Math.random() < 0.5) {
+        step({
+          start: end,
+          length: b.length,
+          theta: b.theta + 0.2
+        })
+      }
+    }
+
+    step({
+      start: { x: WIDTH / 2, y: HEIGHT },
+      length: 40,
+      theta: -Math.PI / 2
+    })
+  }
+
+  useEffect(() => {
+    init()
+  }, [])
+
   return (
     <>
-      <div>hi</div>
+      <canvas
+        ref={canvasRef}
+        width='600'
+        height='600'
+        style={{ border: '1px solid' }}
+      />
     </>
   )
 }
